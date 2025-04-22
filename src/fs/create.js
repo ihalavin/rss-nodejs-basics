6e1b5319bb3daee0950e3df413648bf75b2d509e
Ilya Golovin <foreachq@gmail.com>
2025-04-22 21:46:05 +0300

    feat: implementation of create.js task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,11 @@
+import * as fs from 'node:fs';
+
 const create = async () => {
-    // Write your code here 
+    fs.writeFile('files/fresh.txt', 'I am fresh and young', {flag: 'wx'}, err => {
+        if (err) {
+            throw new Error('FS operation failed');
+        }
+    });
 };
 
 await create();
