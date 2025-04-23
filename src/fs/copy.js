3aa9490f7aa46d459d5015fd0fcf3ab3a76f38a0
Ilya Golovin <foreachq@gmail.com>
2025-04-23 16:29:12 +0300

    feat: implementation of copy.js task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,11 @@
+import fs from 'node:fs';
+
 const copy = async () => {
-    // Write your code here 
+    fs.cp('files', 'files_copy', {force: false, errorOnExist: true, recursive: true}, err => {
+        if (err) {
+            throw new Error('FS operation failed');
+        }
+    });
 };
 
 await copy();
