88bfd060472ccb8228e045557b88817fd406fac5
Ilya Golovin <foreachq@gmail.com>
2025-04-24 22:33:03 +0300

    feat: implementation of rename.js task

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,29 @@
+import {access, rename as renameFile} from 'node:fs/promises';
+
 const rename = async () => {
-    // Write your code here 
+    const __dirname = import.meta.dirname;
+    const newPath = __dirname + '/files/properFilename.md';
+    const oldPath = __dirname + '/files/wrongFilename.txt';
+    const fsErrorMessage = 'FS operation failed';
+
+    await access(newPath)
+        .then(() => {
+            throw new Error(fsErrorMessage);
+        })
+        .catch((err) => {
+            if (err.message === fsErrorMessage) {
+                throw err;
+            }
+        });
+
+    await access(oldPath).catch(() => {
+        throw new Error(fsErrorMessage);
+    });
+
+    renameFile(oldPath, newPath)
+        .catch(() => {
+            throw new Error(fsErrorMessage);
+        });
 };
 
 await rename();
