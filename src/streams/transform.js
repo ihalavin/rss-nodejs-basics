40e4ef3a6685d89f69e1f7a6e196ba8f8d9bdab0
Ilya Golovin <foreachq@gmail.com>
2025-04-28 20:53:20 +0300

    feat: implementation of streams/transform.js task

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,16 @@
+import {Transform} from 'node:stream';
+
 const transform = async () => {
-    // Write your code here 
+    const reverseStream = new Transform({
+        transform(chunk, encoding, callback) {
+            const reverse = chunk.toString().split('').reverse().join('') + '\n';
+            callback(null, reverse);
+        }
+    });
+
+    process.stdin
+        .pipe(reverseStream)
+        .pipe(process.stdout);
 };
 
 await transform();
