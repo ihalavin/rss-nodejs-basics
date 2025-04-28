bea38b903d6daff81fd78d1dc8197a9eea5f82d1
Ilya Golovin <foreachq@gmail.com>
2025-04-28 19:54:03 +0300

    feat: implementation of calcHash.js task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+import {createHash} from 'node:crypto';
+import {createReadStream} from 'node:fs';
+import {join} from 'node:path';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const __dirname = import.meta.dirname;
+    const filePath = join(__dirname, '/files/fileToCalculateHashFor.txt');
+    const fileStream = createReadStream(filePath);
+
+    let hash = createHash('SHA256');
+
+    fileStream
+        .pipe(hash)
+        .on('finish', () => {
+            console.log(hash.digest('hex'));
+        })
 };
 
 await calculateHash();
