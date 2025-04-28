2980b29747175af6e8705ad5d8ca548d135b8b6c
Ilya Golovin <foreachq@gmail.com>
2025-04-28 20:39:05 +0300

    feat: implementation of streams/read.js task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
+import {createReadStream} from 'node:fs';
+import {join} from 'node:path';
+
 const read = async () => {
-    // Write your code here 
+    const __dirname = import.meta.dirname;
+    const filePath = join(__dirname, '/files/fileToRead.txt');
+    const fileReadStream = createReadStream(filePath);
+
+    fileReadStream
+        .pipe(process.stdout)
+        .on('unpipe', () => {
+            console.log();
+        });
 };
 
 await read();
